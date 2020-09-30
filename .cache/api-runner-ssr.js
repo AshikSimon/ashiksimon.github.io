945b250265c123fad7c826f8483b24fd3191b616
AshikSimon <ashiksimon@gmail.com>
2020-09-30 14:27:54 -0700

    install gatsby?

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,8 +1,8 @@
 var plugins = [{
-      plugin: require('C:/Users/Ashik/Documents/GitHub/portfolio/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('C:/Users/Ashik/Documents/GitHub/ashiksimon.github.io/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('C:/Users/Ashik/Documents/GitHub/portfolio/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('C:/Users/Ashik/Documents/GitHub/ashiksimon.github.io/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":null},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,10 +5,9 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Ashik\\Documents\\GitHub\\portfolio\\.cache\\dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\Ashik\\Documents\\GitHub\\portfolio\\src\\pages\\404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Ashik\\Documents\\GitHub\\portfolio\\src\\pages\\index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("C:\\Users\\Ashik\\Documents\\GitHub\\portfolio\\src\\pages\\page-2.js"))),
-  "component---src-pages-yonder-dynamics-arm-js": hot(preferDefault(require("C:\\Users\\Ashik\\Documents\\GitHub\\portfolio\\src\\pages\\yonder-dynamics-arm.js")))
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\Ashik\\Documents\\GitHub\\ashiksimon.github.io\\src\\pages\\404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Ashik\\Documents\\GitHub\\ashiksimon.github.io\\src\\pages\\index.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("C:\\Users\\Ashik\\Documents\\GitHub\\ashiksimon.github.io\\src\\pages\\page-2.js"))),
+  "component---src-pages-yonder-dynamics-arm-js": hot(preferDefault(require("C:\\Users\\Ashik\\Documents\\GitHub\\ashiksimon.github.io\\src\\pages\\yonder-dynamics-arm.js")))
 }
 
