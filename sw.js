af6760d39ab2444abd70a5b4b21cdc7047f5d1fc
AshikSimon <ashiksimon@gmail.com>
2021-01-16 11:37:15 -0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-b34272415d1ab2382561.js"
+    "url": "webpack-runtime-506b1711d36e42bcb6df.js"
   },
   {
     "url": "framework-62aa192001dd5d50f28d.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2788d223eaf46ea39ead9063436113df"
+    "revision": "f856bfcbbd3628b1575776f69f81e198"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-241020a1cf7d0f317bbe.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "d505396ebb55d3e6db30688cce50f515"
+    "revision": "f9cefad88eadfb764bdce0b7d5f7646f"
   },
   {
     "url": "polyfill-795e729a01fe2d5b284f.js"
